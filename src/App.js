574793320042ad69d0fcede8a6dd478911797fa1
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-05 16:57:30 -0700

    Add App basic functionality and fetch data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./styles/main.scss";
+import Aside from "./Aside";
+import LandingPage from "./LandingPage";
+import Header from "./Header";
+import Question from "./Question";
+// import { }
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      userName: "",
+      techQuestions: [],
+      randomQuestions: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://memoize-datasets.herokuapp.com/api/v1/interviewQuestions")
+      .then(interviewQuestions => interviewQuestions.json())
+      .then(interviewQuestions => {
+        this.setState({
+          techQuestions: interviewQuestions.interviewQuestions.technical,
+          randomQuestions: interviewQuestions.interviewQuestions.random
+        });
+      })
+      .catch(err => console.log("cards error", err));
+  }
+
+  updateUserName = userName => {
+    this.setState({ userName });
+  };
+
+  renderLandingPage() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
+        <LandingPage updateUserName={this.updateUserName} />
       </div>
     );
+  }
+
+  renderApp() {
+    const { userName, techQuestions, randomQuestions } = this.state;
+    return (
+      <div className="app">
+        <Aside userName={userName} />
+        <div>
+          <Header />
+          <Question
+            techQuestions={techQuestions}
+            randomQuestions={randomQuestions}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return this.state.userName === ""
+      ? this.renderLandingPage()
+      : this.renderApp();
   }
 }
 
