8e8c6c5bd0dd0e6c1af73b21f5a5800badd31297
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-07 18:37:58 -0700

    Add AnswerCard rendering and saving to local storage

--- src/Question.js
+++ src/Question.js
@@ -1,36 +1,66 @@
 import React, { Component } from "react";
 import generateRandomNumber from "./utils/helper";
+import AnswerCard from "./AnswerCard";
 
 class Question extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentQuestion: {},
-      currentAnswer: ""
+      currentAnswer: "",
+      isSubmitted: false
     };
   }
 
-  getRandomQuestion(props, randomIndex) {
-    const question = this.props.techQuestions.slice(randomIndex, 1);
+  getRandomQuestion(randomIndex) {
+    const question = [...this.props.techQuestions].splice(randomIndex, 1);
     return question;
   }
 
-  componentDidMount(props) {
+  updateCurrentAnswer = event => {
+    this.setState({ currentAnswer: event.target.value });
+  };
+
+  componentWillMount() {
     const randomNum = generateRandomNumber(this.props.techQuestions.length);
-    const [...question] = this.getRandomQuestion(randomNum);
+    const question = this.getRandomQuestion(randomNum);
     this.setState({ currentQuestion: question });
   }
 
+  toggleSubmitAnswer = event => {
+    event.preventDefault();
+    this.setState({ isSubmitted: true }, () => {
+      localStorage.setItem('answers', {
+        question: this.state.currentQuestion[0].question,
+        answer: this.state.currentAnswer
+      });
+    });
+  };
+
   render() {
-    return (
-      <div>
-        <h2>{this.state.currentQuestion.question}</h2>
-        <form>
-          <input />
-          <button>Save Answer</button>
-        </form>
-      </div>
-    );
+    const { isSubmitted, currentAnswer, currentQuestion } = this.state;
+    if (isSubmitted) {
+      return (
+        <AnswerCard
+          currentAnswer={currentAnswer}
+          currentQuestion={currentQuestion}
+        />
+      );
+    } else {
+      return (
+        <div>
+          <h2>{currentQuestion[0].question}</h2>
+          <form onSubmit={this.toggleSubmitAnswer}>
+            <textarea
+              type="text"
+              value={this.state.value}
+              onChange={this.updateCurrentAnswer}
+            />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      );
+    }
   }
 }
 
