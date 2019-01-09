e399cabcf028442a335729a5c1f054e344851270
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-09 00:51:09 -0700

    Add App tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let userName = "Hillary";
+
+describe("App", () => {
+  let wrapper;
+  beforeEach(() => {
+    localStorage.setItem('savedAnswers', '[1, 2, 3]');
+    wrapper = shallow(<App />);
+  });
+
+  afterEach(() => {
+    localStorage.clear();
+  });
+
+  it("should match snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should setState when updateUserName is called", () => {
+    expect(wrapper.state("userName").length).toEqual(0);
+    wrapper.instance().updateUserName(userName);
+    expect(wrapper.state("userName").length).toEqual(7);
+  });
+
+
+
+
 });
