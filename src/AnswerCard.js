1273a7c2fc3bd6b82d78bd10f78e09a541462c32
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-13 15:31:25 -0700

    Change AnswerCard to class component and add createAnswerPackage method

--- src/AnswerCard.js
+++ src/AnswerCard.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function AnswerCard(props) {
-  const { question, link, video } = props.currentQuestion[0];
-  return <div className="answerCard-div">
-      <h2>{question}</h2>
-      <p>{props.currentAnswer}</p>
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        Article about question
-      </a>
-      <br />
-      <a href={video} target="_blank" rel="noopener noreferrer">
-        Video about question
-      </a>
-    </div>;
+class AnswerCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  createAnswerPackage = () => {
+    let answer = {
+      question: this.props.techQuestions[this.props.currentQuestionIndex],
+      answer: this.props.currentAnswer
+    };
+    this.props.updateSavedAnswers(answer);
+  };
+
+  render() {
+    const { question, link, video } = this.props.techQuestions[this.props.currentQuestionIndex];
+    return (
+      <div className="answerCard-div">
+        <h2>{question}</h2>
+        <p>{this.props.currentAnswer}</p>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          Article about question
+        </a>
+        <br />
+        <a href={video} target="_blank" rel="noopener noreferrer">
+          Video about question
+        </a>
+        <button onClick={this.createAnswerPackage}>Save Answer</button>
+      </div>
+    );
+  }
 }
+
+export default AnswerCard;
